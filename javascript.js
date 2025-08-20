d59affc42ae6f2e5845f697deafc667ee4ef5b0e
Annie Sheen <anniedsheen@gmail.com>
2025-08-20 15:09:07 -0500

    Add score incrementing to playRound function
    Implement logic that updates humanScore and computerScore variables based on the round winner.

--- javascript.js
+++ javascript.js
@@ -25,16 +25,22 @@ function playRound(humanChoice, computerChoice) {
             console.log ("It's a draw!");
         } else if (humanChoice === "rock" && computerChoice === "paper") {
             console.log ("You lose! Paper beats rock.");
+            computerScore++;
         } else if (humanChoice === "scissors" && computerChoice === "paper") {
             console.log ("You win! Scissors beats paper.");
+            humanScore++;
         } else if (humanChoice === "paper" && computerChoice === "rock") {
             console.log ("You win! Paper beats rock.");
+            humanScore++;
         } else if (humanChoice === "paper" && computerChoice === "scissors") {
             console.log ("You lose! Scissors beats paper.");
+            computerScore++;
         } else if (humanChoice === "rock" && computerChoice === "scissors") {
             console.log ("You win! Rock beats scissors.");
+            humanScore++;
         } else if (humanChoice === "scissors" && computerChoice === "rock") {
             console.log ("You lose! Rock beats scissors.");
+            computerScore++;
         } 
 }
 
